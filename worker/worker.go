322132e0808947b8807dd79b836c92c2a135fd48
BeLeap <beleap@beleap.dev>
2024-09-22 17:32:01 +0900

    add Worker

---
+++ worker/worker.go
@@ -0,0 +1,14 @@
+package worker
+
+import (
+	"beleap.dev/cube/task"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+)
+
+type Worker struct {
+  Name string
+  Queue queue.Queue
+  Db map[uuid.UUID]*task.Task
+  TaskCount int
+}
