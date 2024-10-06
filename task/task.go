c419796e2518995a6bbb4b9782faa1328c5a7a4c
BeLeap <beleap@beleap.dev>
2024-10-06 21:49:27 +0900

    add struct

--- task/task.go
+++ task/task.go
@@ -3,6 +3,7 @@ package task
 import (
 	"time"
 
+	"github.com/docker/docker/client"
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
 )
@@ -51,4 +52,16 @@ type Config struct {
 	Disk          int64
 	Env           []string
 	RestartPolicy string
+}
+
+type Docker struct {
+	Client *client.Client
+	Config Config
+}
+
+type DockerResult struct {
+	Error       error
+	Action      string
+	ContainerId string
+	Result      string
 }
