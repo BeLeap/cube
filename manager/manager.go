0cd374f645bdc7a96816751c70fc0c73e080ba5b
BeLeap <beleap@beleap.dev>
2024-10-06 21:09:00 +0900

    dummy methods

--- manager/manager.go
+++ manager/manager.go
@@ -1,6 +1,8 @@
 package manager
 
 import (
+	"fmt"
+
 	"beleap.dev/cube/task"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
@@ -13,4 +15,16 @@ type Manager struct {
 	Workers       []string
 	WorkerTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {
+	fmt.Println("I will select an appropriate worker.")
+}
+
+func (m *Manager) UpdateTasks() {
+	fmt.Println("I will update tasks.")
+}
+
+func (m *Manager) SendWork() {
+	fmt.Println("I will send work to workers")
 }
