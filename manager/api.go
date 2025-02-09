0dd83e97b7496fba636d44249d154f4924bb9e0e
BeLeap <beleap@beleap.dev>
2025-02-09 19:27:07 +0900

    [8.2] add StartTaskHandler

--- manager/api.go
+++ manager/api.go
@@ -1,10 +1,49 @@
 package manager
 
-import "github.com/go-chi/chi/v5"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"beleap.dev/cube/task"
+	"github.com/go-chi/chi/v5"
+)
 
 type Api struct {
 	Address string
 	Port    int
 	Manager *Manager
 	Router  *chi.Mux
+}
+
+type ErrResponse struct {
+	HTTPStatusCode int
+	Message        string
+}
+
+func (a *Api) StartTaskHandler(w http.ResponseWriter, r *http.Request) {
+	d := json.NewDecoder(r.Body)
+	d.DisallowUnknownFields()
+
+	te := task.TaskEvent{}
+	err := d.Decode(&te)
+	if err != nil {
+		msg := fmt.Sprintf("Error unmarshalling body: %v\n", err)
+		log.Printf(msg)
+		w.WriteHeader(400)
+		e := ErrResponse{
+			HTTPStatusCode: 400,
+			Message:        msg,
+		}
+		json.NewEncoder(w).Encode(e)
+		return
+	}
+
+	a.Manager.AddTask(te)
+
+	log.Printf("Added task %v\n", te.Task.ID)
+	w.WriteHeader(201)
+
+	json.NewEncoder(w).Encode(te.Task)
 }
