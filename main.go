200fd8ab931181cc23549a757a314e8faf337c56
BeLeap <beleap@beleap.dev>
2025-02-16 16:17:42 +0900

    [8.8] move runTasks to worker

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 		Worker:  &w,
 	}
 
-	go runTasks(&w)
+	go w.RunTasks()
 	go w.CollectStats()
 	go api.Start()
 
@@ -66,20 +66,5 @@ func main() {
 			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
 			time.Sleep(15 * time.Second)
 		}
-	}
-}
-
-func runTasks(w *worker.Worker) {
-	for {
-		if w.Queue.Len() != 0 {
-			result := w.RunTask()
-			if result.Error != nil {
-				log.Printf("Error running task: %v\n", result.Error)
-			}
-		} else {
-			log.Print("No tasks to process currently.\n")
-		}
-		log.Println("Sleeping for 10 seconds.")
-		time.Sleep(10 * time.Second)
 	}
 }
