afc854eb9c2e384a1d004679385c1738790e1b13
BeLeap <beleap@beleap.dev>
2025-03-09 23:00:09 +0900

    [10.4] implement score
    [10.4] implement score

--- scheduler/roundrobin.go
+++ scheduler/roundrobin.go
@@ -21,10 +21,28 @@ func (r *RoundRobin) Pick(scores map[string]float64, candidates []*node.Node) *n
 
 // Score implements Scheduler.
 func (r *RoundRobin) Score(t task.Task, nodes []*node.Node) map[string]float64 {
-	panic("unimplemented")
+	nodeScores := make(map[string]float64)
+	var newWorker int
+	if r.LastWorker+1 < len(nodes) {
+		newWorker = r.LastWorker + 1
+		r.LastWorker++
+	} else {
+		newWorker = 0
+		r.LastWorker = 0
+	}
+
+	for idx, node := range nodes {
+		if idx == newWorker {
+			nodeScores[node.Name] = 0.1
+		} else {
+			nodeScores[node.Name] = 1.0
+		}
+	}
+
+	return nodeScores
 }
 
 // SelectCandidateNodes implements Scheduler.
 func (r *RoundRobin) SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node {
-	panic("unimplemented")
+	return nodes
 }
